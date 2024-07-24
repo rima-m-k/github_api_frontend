ec9dd99758d32d0ed5fab112c1f0539f8a6400e5
rima-m-k <rimamethalamkandy@gmail.com>
2024-07-24 08:25:27 +0530

    user data display changes

--- src/pages/RepoList/RepoList.jsx
+++ src/pages/RepoList/RepoList.jsx
@@ -26,7 +26,7 @@ function RepoList() {
     dispatch(setCurrentRepo(repoName));
     navigate(`/repository-details`);
   };
-  console.log(repos)
+  console.log(repos);
 
   if (!userData) {
     return <div>Loading...</div>; // or a suitable loading indicator
@@ -34,40 +34,50 @@ function RepoList() {
 
   return (
     <div>
-    
-
       <div className="user-profile">
         <div className="user-header">
           <img
             src={userData.avatar_url}
             alt={`${userData.name}'s avatar`}
             className="user-avatar"
-            />
-            <Link to="/followers-list">View followers</Link>
-
+          />
+          <Link to="/followers-list">View followers</Link>
         </div>
         <div className="user-details">
-        <h1>{userData.name}</h1>
+          <h1>{userData.name}</h1>
+          {userData.bio && (
+            <p>
+              <strong>Bio:</strong> {userData.bio}
+            </p>
+          )}
 
-          <p>
-            <strong>Bio:</strong> {userData.bio}
-          </p>
-          <p>
-            <strong>Blog:</strong>{" "}
-            <a href={userData.blog} target="_blank" rel="noopener noreferrer">
-              {userData.blog}
-            </a>
-          </p>
-          <p>
-            <strong>Company:</strong> {userData.company}
-          </p>
-          <p>
-            <strong>Email:</strong>{" "}
-            <a href={`mailto:${userData.email}`}>{userData.email}</a>
-          </p>
-          <p>
-            <strong>Location:</strong> {userData.location}
-          </p>
+          {userData.blog && (
+            <p>
+              <strong>Blog:</strong>{" "}
+              <a href={userData.blog} target="_blank" rel="noopener noreferrer">
+                {userData.blog}
+              </a>
+            </p>
+          )}
+
+          {userData.company && (
+            <p>
+              <strong>Company:</strong> {userData.company}
+            </p>
+          )}
+
+          {userData.email && (
+            <p>
+              <strong>Email:</strong>{" "}
+              <a href={`mailto:${userData.email}`}>{userData.email}</a>
+            </p>
+          )}
+          {userData.location && (
+            <p>
+              <strong>Location:</strong> {userData.location}
+            </p>
+          )}
+
           <p>
             <strong>Followers:</strong>
             {userData.followers}
@@ -80,32 +90,39 @@ function RepoList() {
       </div>
 
       <div className="repositories">
-      <h2>Repositories</h2>
-      {repos && repos.length > 0 ? (
-        <div className="repository-cards">
-          {repos.map((repo) => (
-            <div 
-              key={repo.id} 
-              className="repository-card" 
-              onClick={() => handleRepository(repo.name)}
-            >
-              <div className="card-header">
-                <img src={repo.owner.avatar_url} alt={`${repo.owner.login}'s avatar`} className="repo-avatar" />
-                <h3>{repo.name}</h3>
+        <h2>Repositories</h2>
+        {repos && repos.length > 0 ? (
+          <div className="repository-cards">
+            {repos.map((repo) => (
+              <div
+                key={repo.id}
+                className="repository-card"
+                onClick={() => handleRepository(repo.name)}
+              >
+                <div className="card-header">
+                  <img
+                    src={repo.owner.avatar_url}
+                    alt={`${repo.owner.login}'s avatar`}
+                    className="repo-avatar"
+                  />
+                  <h3>{repo.name}</h3>
+                </div>
+                <p className="repo-description">{repo.description}</p>
+                <div className="repo-topics">
+                  {repo.topics &&
+                    repo.topics.map((topic, index) => (
+                      <span key={index} className="repo-topic">
+                        {topic}
+                      </span>
+                    ))}
+                </div>
               </div>
-              <p className="repo-description">{repo.description}</p>
-              <div className="repo-topics">
-                {repo.topics && repo.topics.map((topic, index) => (
-                  <span key={index} className="repo-topic">{topic}</span>
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
-      ) : (
-        <div>No repositories found.</div>
-      )}
-    </div>
+            ))}
+          </div>
+        ) : (
+          <div>No repositories found.</div>
+        )}
+      </div>
     </div>
   );
 }
